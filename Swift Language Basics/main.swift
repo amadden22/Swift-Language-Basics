134bf885f41ac977f315a1ae3ce207ed634d0cd6
Aidan Madden <aidanmadden@mwc-031075.dhcp.missouri.edu>
2017-09-07 14:05:57 -0500

    Applied .gitignore

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Aidan Madden on 9/7/17.
+//  Copyright © 2017 Aidan Madden. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
